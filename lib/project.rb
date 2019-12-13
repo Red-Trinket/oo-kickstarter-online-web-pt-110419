f2550a8e080734e29147b281366b0166d22b0eaf
Matthew Jones <matts3371@gmail.com>
2019-12-13 23:26:21 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 class Project 
   attr_accessor :name 
   attr_reader :backers 
-  @backers = []
   
   def initialize(name)
     @name = name 
+    @backers = []
   end 
   
   def add_backer(backer)
